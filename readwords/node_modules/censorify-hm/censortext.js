780b8f9227db4e4ae7306636d69ea6424266f650
Harish Malik <harish.malik@asu.edu>
2016-07-07 17:48:49 -0700

    Adding README.md

--- readwords/node_modules/censorify-hm/censortext.js
+++ readwords/node_modules/censorify-hm/censortext.js
@@ -3,7 +3,7 @@ var customCensoredWords = [];
 
 function censor(inStr) {
 	for(idx in censoredWords){
-		inStr = inStr.replace(customCensoredWords[idx], "****");
+		inStr = inStr.replace(censoredWords[idx], "****");
 	}
 
 	for(idx in customCensoredWords){
